6cad69054ce5d31800391e2bdb83d5a115c37f38
DigitalDominance <digitaldominanceseo@gmail.com>
2025-06-10 17:47:44 -0400

    Update ZKResume.sol

--- contracts/ZKResume.sol
+++ contracts/ZKResume.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
+pragma solidity ^0.8.20;
 
 interface IZKVerifier {
     function verifyProof(bytes calldata proof, uint256[] calldata pubSignals) external view returns (bool);
