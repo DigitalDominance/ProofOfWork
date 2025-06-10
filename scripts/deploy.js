b1a86059502d29a7ca485e899ffbcd0924b033d9
DigitalDominance <digitaldominanceseo@gmail.com>
2025-06-10 18:28:04 -0400

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,29 +1,23 @@
-// scripts/deploy.js
-
 const hre = require("hardhat");
 
 async function main() {
-  // 1) Get the deployer signer
   const [deployer] = await hre.ethers.getSigners();
   console.log("Deploying with:", deployer.address);
 
-  // 2) Get contract factory for "JobFactory"
   const Factory = await hre.ethers.getContractFactory("JobFactory");
+  console.log("Contract factory loaded:", Factory.interface.fragments.map(f => f.name || f.type));
 
-  // 3) Deploy the contract, passing the deployer's address into its constructor
   const factory = await Factory.deploy(deployer.address);
+  const tx = factory.deploymentTransaction();
+  console.log("Deployment TX hash:", tx.hash);
 
-  // 4) WAIT for the transaction to be mined and the contract to be on-chain
   await factory.waitForDeployment();
-
   console.log("JobFactory deployed at:", factory.target);
-  // In Ethers v6, `factory.target` is the contract address
-  // (Equivalent to v5's `factory.address`)
 }
 
 main()
   .then(() => process.exit(0))
   .catch(err => {
-    console.error(err);
+    console.error("Deployment error:", err);
     process.exit(1);
   });
