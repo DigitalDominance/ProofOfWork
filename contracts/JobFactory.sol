ffca98e80a4a843894a89a04ac4ebec6d728ff5d
DigitalDominance <digitaldominanceseo@gmail.com>
2025-06-10 17:43:36 -0400

    Add files via upload

--- contracts/JobFactory.sol
+++ contracts/JobFactory.sol
@@ -1,57 +1,40 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
+pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "./ProofOfWorkJob.sol";
 
-/// @notice Factory that deploys and tracks all ProofOfWorkJob instances
 contract JobFactory is Ownable, ReentrancyGuard {
     event JobCreated(address indexed jobAddress, address indexed employer);
 
-    /// @dev platform fee in basis points (75 = 0.75%)
-    uint256 public constant PLATFORM_FEE_BPS = 75;
-    address payable public feeRecipient;
+    constructor() Ownable(msg.sender) {}
 
-    constructor(address payable _feeRecipient) {
-        require(_feeRecipient != address(0), "Bad fee recipient");
-        feeRecipient = _feeRecipient;
-    }
-
-    /// @notice Create a new ProofOfWorkJob
     function createJob(
-        uint256 minStake,
-        uint256 duration,
-        uint256 reward,
-        address payable worker,
-        address payable employer
-    ) external nonReentrant onlyOwner returns (address) {
+        address _employer,
+        uint8 _payType,
+        uint256 _weeklyPay,
+        uint256 _durationWeeks,
+        uint256 _totalPay,
+        string memory _title,
+        string memory _description,
+        uint256 _positions
+    ) external onlyOwner returns (address) {
         ProofOfWorkJob job = new ProofOfWorkJob(
-            minStake,
-            duration,
-            reward,
-            worker,
-            employer,
-            feeRecipient
+            _employer,
+            _payType,
+            _weeklyPay,
+            _durationWeeks,
+            _totalPay,
+            _title,
+            _description,
+            _positions
         );
-        address jobAddr = address(job);
-        jobsByEmployer[msg.sender].push(jobAddr);
-        allJobs.push(jobAddr);
-        emit JobCreated(jobAddr, msg.sender);
-        return jobAddr;
-    }
-
-    /// @notice Total jobs count
-    function totalJobs() external view returns (uint256) {
-        return allJobs.length;
-    }
-
-    /// @notice Jobs by employer
-    function getJobsByEmployer(address employer) external view returns (address[] memory) {
-        return jobsByEmployer[employer];
+        allJobs.push(address(job));
+        emit JobCreated(address(job), _employer);
+        return address(job);
     }
 
     // Internal storage
     address[] internal allJobs;
-    mapping(address => address[]) internal jobsByEmployer;
 }
--- contracts/ReputationSystem.sol
+++ contracts/ReputationSystem.sol
@@ -1,14 +1,10 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
+pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-/// @notice Tracks employer & worker reputation on-chain
 contract ReputationSystem is Ownable {
-    /// @param initialOwner The job contract that may update scores
-    constructor(address initialOwner) {
-        _transferOwnership(initialOwner);
-    }
+    constructor(address initialOwner) Ownable(initialOwner) {}
 
     mapping(address => uint256) public workerScore;
     mapping(address => uint256) public employerScore;
@@ -16,19 +12,16 @@ contract ReputationSystem is Ownable {
     event WorkerScoreUpdated(address indexed worker, uint256 newScore);
     event EmployerScoreUpdated(address indexed employer, uint256 newScore);
 
-    /// @notice Increment a worker's score
     function updateWorker(address worker, uint256 delta) external onlyOwner {
         workerScore[worker] += delta;
         emit WorkerScoreUpdated(worker, workerScore[worker]);
     }
 
-    /// @notice Increment an employer’s score
     function updateEmployer(address emp, uint256 delta) external onlyOwner {
         employerScore[emp] += delta;
         emit EmployerScoreUpdated(emp, employerScore[emp]);
     }
 
-    /// @notice Fetch both scores in one call
     function getScores(address user) external view returns (uint256 work, uint256 emp) {
         return (workerScore[user], employerScore[user]);
     }
