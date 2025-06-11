68b67f7376bea111afbc404abb2fcd0bc28a276c
DigitalDominance <digitaldominanceseo@gmail.com>
2025-06-10 22:04:57 -0400

    Update DisputeDAO.sol

--- contracts/DisputeDAO.sol
+++ contracts/DisputeDAO.sol
@@ -4,14 +4,18 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
-/// @notice On-chain dispute module with appointed jurors
+interface IProofOfWorkJob {
+    function employer() external view returns (address);
+}
+
 contract DisputeDAO is Ownable, ReentrancyGuard {
     struct Dispute {
         address job;
         address initiator;
-        bool    resolved;
+        bool resolved;
         uint256 votesFor;
         uint256 votesAgainst;
+        string[] messages;
     }
 
     mapping(uint256 => Dispute) public disputes;
@@ -20,69 +24,88 @@ contract DisputeDAO is Ownable, ReentrancyGuard {
     address[] public jurors;
     mapping(address => bool) public isJuror;
 
-    event JurorAdded(address indexed juror);
-    event JurorRemoved(address indexed juror);
     event DisputeCreated(uint256 indexed id, address indexed job, address indexed by);
     event Voted(uint256 indexed id, address indexed juror, bool support);
     event DisputeResolved(uint256 indexed id, bool outcome);
+    event MessagePosted(uint256 indexed id, address indexed sender, string message);
 
-    /// @param initialOwner typically the ProofOfWorkJob contract
-    constructor(address initialOwner) Ownable(initialOwner) {}
-
-    /// @notice Add a juror (only owner)
-    function addJuror(address _juror) external onlyOwner {
-        require(_juror != address(0) && !isJuror[_juror], "Bad juror");
-        isJuror[_juror] = true;
-        jurors.push(_juror);
-        emit JurorAdded(_juror);
+    constructor(address initialOwner) Ownable(initialOwner) {
+        address juror1 = 0xfF817442F4Cc914b0338F197c4c0EfFe2E2707C9;
+        address juror2 = 0xA0c5048c32870bB66d0BE861643cD6Bb5F66Ada2;
+        jurors.push(juror1);
+        jurors.push(juror2);
+        isJuror[juror1] = true;
+        isJuror[juror2] = true;
     }
 
-    /// @notice Remove a juror (only owner)
-    function removeJuror(address _juror) external onlyOwner {
-        require(isJuror[_juror], "Not juror");
-        isJuror[_juror] = false;
-        for (uint i = 0; i < jurors.length; i++) {
-            if (jurors[i] == _juror) {
-                jurors[i] = jurors[jurors.length - 1];
-                jurors.pop();
-                break;
-            }
-        }
-        emit JurorRemoved(_juror);
-    }
-
-    /// @notice List all jurors
     function getJurors() external view returns (address[] memory) {
         return jurors;
     }
 
-    /// @notice Initiate a dispute (anyone)
-    function createDispute(address job, uint256 /*jobId*/) external returns (uint256) {
+    function createDispute(address job) external returns (uint256) {
+        address employer = IProofOfWorkJob(job).employer();
+        require(msg.sender == employer || msg.sender == tx.origin, "Not authorized");
+
         Dispute storage d = disputes[disputeCount];
-        d.job        = job;
-        d.initiator  = msg.sender;
-        d.resolved   = false;
-        d.votesFor   = 0;
+        d.job = job;
+        d.initiator = msg.sender;
+        d.resolved = false;
+        d.votesFor = 0;
         d.votesAgainst = 0;
         emit DisputeCreated(disputeCount, job, msg.sender);
         return disputeCount++;
     }
 
-    /// @notice Cast a vote (appointed jurors only)
+    function postMessage(uint256 id, string calldata message) external {
+        Dispute storage d = disputes[id];
+        require(!d.resolved, "Closed");
+
+        address employer = IProofOfWorkJob(d.job).employer();
+        require(
+            msg.sender == employer || msg.sender == d.initiator || isJuror[msg.sender],
+            "Not allowed"
+        );
+
+        d.messages.push(message);
+        emit MessagePosted(id, msg.sender, message);
+    }
+
+    function getMessages(uint256 id) external view returns (string[] memory) {
+        return disputes[id].messages;
+    }
+
     function vote(uint256 id, bool support) external {
         require(isJuror[msg.sender], "Not juror");
         Dispute storage d = disputes[id];
         require(!d.resolved, "Already resolved");
-        if (support) d.votesFor++; else d.votesAgainst++;
+
+        if (support) d.votesFor++;
+        else d.votesAgainst++;
+
         emit Voted(id, msg.sender, support);
     }
 
-    /// @notice Finalize once voting is complete
     function finalize(uint256 id) external nonReentrant {
         Dispute storage d = disputes[id];
-        require(!d.resolved, "Done");
+        require(!d.resolved, "Already done");
         d.resolved = true;
         bool outcome = d.votesFor > d.votesAgainst;
         emit DisputeResolved(id, outcome);
     }
+
+    function getDisputeSummary(uint256 id) external view returns (
+        address job,
+        address initiator,
+        bool resolved,
+        uint256 votesFor,
+        uint256 votesAgainst
+    ) {
+        Dispute storage d = disputes[id];
+        return (d.job, d.initiator, d.resolved, d.votesFor, d.votesAgainst);
+    }
+
+    function getDisputeCount() external view returns (uint256) {
+        return disputeCount;
+    }
+
 }
